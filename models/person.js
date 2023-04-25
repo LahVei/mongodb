e5885b6588c65f1fad654aa4f0b9e7f5de54718d
LahVei <fellow90@hotmail.com>
2023-04-25 20:18:44 +0300

    URL Mongo

--- models/person.js
+++ models/person.js
@@ -5,7 +5,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', process.env.MONGODB_URI)
 console.log('connecting to', process.env.PORT)
 //mongodb+srv://milky:felDB@milkyway.sgse8eg.mongodb.net/Phonebook?retryWrites=true&w=majority
-mongoose.connect(url)
+mongoose.connect('mongodb+srv://milky:felDB@milkyway.sgse8eg.mongodb.net/Phonebook?retryWrites=true&w=majority')
 .then(result => {
   console.log('connected to MongoDB')
 })
